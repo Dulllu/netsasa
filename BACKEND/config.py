150fa7bccd320ae1d5e4226595e58ffbeac6fe8b
dulu <151961739+Dulllu@users.noreply.github.com>
2025-10-25 21:03:19 +0300

    Delete BACKEND directory

--- BACKEND/config.py
+++
@@ -1,6 +0,0 @@
-# Safaricom Sandbox Credentials
-CONSUMER_KEY = "3M35EncHJmlL680JFPmJXZEZ7Wwx6i6ekwcnpCK5s7uKxnoE"
-CONSUMER_SECRET = "lP0SKG8chzsNQ3ChP3noNkWEAZGc4AXN8XtbIjWHpdTPB08A9DUVcEQggZIMpFg5"
-SHORTCODE = "174379"  # Default test Paybill
-PASSKEY = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
-CALLBACK_URL = "https://4121dd64e105.ngrok-free.app/api/callback"  # or your own endpoint if public
